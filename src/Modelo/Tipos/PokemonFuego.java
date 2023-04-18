84318d999b4e4380953d6e874baf053b24b129cb
AsierCardoso <104497993+AsierCardoso@users.noreply.github.com>
2023-04-18 18:09:23 +0200

    Update PokemonFuego.java

--- src/Modelo/Tipos/PokemonFuego.java
+++ src/Modelo/Tipos/PokemonFuego.java
@@ -11,7 +11,7 @@ public class PokemonFuego extends Pokemon{
 	
 	protected int aumentarAtaque(Tipo tipo) {
 		int aumento = 1;
-		if (tipo.equals(Tipo.AGUA) || tipo.equals(Tipo.ROCA)) {
+		if (tipo.equals(Tipo.HIELO) || tipo.equals(Tipo.PLANTA)||tipo.equals(Tipo.BICHO) || tipo.equals(Tipo.ACERO)) {
 			aumento = this.random.nextInt(2)+2;
 		}
 		return aumento;
